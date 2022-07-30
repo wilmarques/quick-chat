86071f63e73127ed2da7f2df836f96e6fc4af7a4
wilmarques <wileymarques@live.com>
2022-07-30 02:28:36 +0000

    ci: Changing deploying method to not lose Github Pages config
    - Before I had to enable https on every deployment
    - Because the branch were being deleted prior to the deployment
    - Also, the subtree got added to main repo

--- deploy-to-ghpages.sh
+++ deploy-to-ghpages.sh
@@ -1,7 +1,5 @@
-rm -rf build
-git push --delete origin gh-pages
 flutter build web --base-href /quick-chat/
 git add -f build/web
-git commit -m "Deploying"
+git subtree pull --prefix build/web origin gh-pages
+git commit -m "ci: Deploying"
 git subtree push --prefix build/web origin gh-pages
-git reset HEAD^ --hard
