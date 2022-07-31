e8fe8d637c41c77c78935f6de509103d44759e38
wilmarques <wileymarques@live.com>
2022-07-31 02:03:52 +0000

    ci: Fixing deploy script

--- deploy-to-ghpages.sh
+++ deploy-to-ghpages.sh
@@ -1,5 +1,6 @@
+git subtree add --prefix build/web origin gh-pages
 flutter build web --base-href /quick-chat/
 git add -f build/web
-git subtree pull --prefix build/web origin gh-pages
 git commit -m "ci: Deploying"
 git subtree push --prefix build/web origin gh-pages
+git reset HEAD~2 --hard
