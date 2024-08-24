8cc01ce892f6be76682700d0e6f57b6a89ab24f2
Wil Marques <wileymarques@live.com>
2024-08-24 13:51:19 +0000

    ci: Improving deploy to GH pages script

--- deploy-to-ghpages.sh
+++ deploy-to-ghpages.sh
@@ -1,9 +1,9 @@
 flutter build web --base-href /quick-chat/
 cd build/web
 git init
-git remote add gh-pages https://github.com/wilmarques/quick-chat.git
-git fetch --all
+git remote add origin https://github.com/wilmarques/quick-chat.git
+git fetch
+git checkout -b gh-pages
 git add -A
 git commit -m "ci: Deploying"
-git checkout -b gh-pages
-git push gh-pages gh-pages
+git push origin gh-pages --force
